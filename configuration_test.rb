ec1054b98fdfa8e4ff2d914f6dcfaf6eb4b8ad66
Kolman <kolmanv@gmail.com>
2011-07-05 19:26:40 +0300

    Crawler small update. Added support for port.

--- configuration_test.rb
+++ configuration_test.rb
@@ -38,7 +38,7 @@ class TestConfiguration < Test::Unit::TestCase
     assert_equal("hd1:+:/*", conf.server_conf_vec[2].directories[0])
     assert_equal([], conf.server_conf_vec[2].servers)
 
-    assert_equal("name:large_server_1\nusername:kuku_crawl\npassword:kaka\ndirectories:\n  hd1:+:/usr/home/bin/*.(mp3|mov)\n  hd1:-:/usr/home/bin/kuku/*\n  hd1:-:/usr/home/bin/*dont_index/*\n  hd2:+:/usr/home/kuku2/file.dot\n  hd2:+:/usr/home/lala/k*\n  hd2:-:/kuku.txt\n  name:small_server_2\n  username:kaka\n  password:\n  directories:\n    tape1:+:/*\n  name:large_server_3\n  username:vasia_crawl_user\n  password:\n  directories:\n    hd1:+:/*\n    hd12:+:/*\n    hd13:+:/*\nname:large_server_4\nusername:vasia_crawl_user\npassword:\ndirectories:\n  hd1:+:/*\nname:large_server_5\nusername:vasia_crawl_user\npassword:\ndirectories:\n  hd1:+:/*\nname:large_server_6\nusername:vasia_crawl_user\npassword:\ndirectories:\n  hd1:+:/*\n", conf.to_s)
+    assert_equal("name:large_server_1\nusername:kuku_crawl\npassword:kaka\nport:2222\ndirectories:\n  hd1:+:/usr/home/bin/*.(mp3|mov)\n  hd1:-:/usr/home/bin/kuku/*\n  hd1:-:/usr/home/bin/*dont_index/*\n  hd2:+:/usr/home/kuku2/file.dot\n  hd2:+:/usr/home/lala/k*\n  hd2:-:/kuku.txt\n  name:small_server_2\n  username:kaka\n  password:\n  port:\n  directories:\n    tape1:+:/*\n  name:large_server_3\n  username:vasia_crawl_user\n  password:\n  port:\n  directories:\n    hd1:+:/*\n    hd12:+:/*\n    hd13:+:/*\nname:large_server_4\nusername:vasia_crawl_user\npassword:\nport:\ndirectories:\n  hd1:+:/*\nname:large_server_5\nusername:vasia_crawl_user\npassword:\nport:\ndirectories:\n  hd1:+:/*\nname:large_server_6\nusername:vasia_crawl_user\npassword:\nport:\ndirectories:\n  hd1:+:/*\n", conf.to_s)
   end
   
   def test_bad_conf
