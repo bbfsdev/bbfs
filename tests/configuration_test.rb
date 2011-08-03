36ff6d5b1829455c756d8aac36cef63f709cf36c
Kolman <kolmanv@gmail.com>
2011-08-03 20:22:41 +0300

    Moving tests and tests resources

--- tests/configuration_test.rb
+++ tests/configuration_test.rb
@@ -3,12 +3,12 @@ require 'test/unit'
 
 class TestConfiguration < Test::Unit::TestCase
   def test_conf_1
-    conf = Configuration.new('configuration_test.1.conf')
+    conf = Configuration.new('resources/configuration_test.1.conf')
     assert_equal(1, conf.server_conf_vec.length)
   end
 
   def test_conf
-    conf = Configuration.new('configuration_test.conf')
+    conf = Configuration.new('resources/configuration_test.conf')
     assert_equal(4, conf.server_conf_vec.length)
     assert_equal("large_server_1", conf.server_conf_vec[0].name)
     assert_equal("kuku_crawl", conf.server_conf_vec[0].username)
@@ -45,9 +45,9 @@ class TestConfiguration < Test::Unit::TestCase
 
     assert_equal("server\n  name:large_server_1\n  username:kuku_crawl\n  password:kaka\n  port:2222\n  directories:\n    hd1:+:/usr/home/bin/*.(mp3|mov)\n    hd1:-:/usr/home/bin/kuku/*\n    hd1:-:/usr/home/bin/*dont_index/*\n    hd2:+:/usr/home/kuku2/file.dot\n    hd2:+:/usr/home/lala/k*\n    hd2:-:/kuku.txt\n  server\n    name:small_server_2\n    username:kaka\n    password:\n    port:\n    directories:\n      tape1:+:/*\n  server\n    name:large_server_3\n    username:vasia_crawl_user\n    password:\n    port:\n    directories:\n      hd1:+:/*\n      hd12:+:/*\n      hd13:+:/*\nserver\n  name:large_server_4\n  username:vasia_crawl_user\n  password:\n  port:\n  directories:\n    hd1:+:/*\nserver\n  name:large_server_5\n  username:vasia_crawl_user\n  password:\n  port:\n  directories:\n    hd1:+:/*\nserver\n  name:large_server_6\n  username:vasia_crawl_user\n  password:\n  port:\n  directories:\n    hd1:+:/*\n", conf.to_s)
   end
-  
+
   def test_bad_conf
-    conf = Configuration.new('configuration_test.bad.conf')
-    assert_equal(nil, conf.server_conf_vec)    
+    conf = Configuration.new('resources/configuration_test.bad.conf')
+    assert_equal(nil, conf.server_conf_vec)
   end
 end
--- tests/content_data_test.rb
+++ tests/content_data_test.rb
@@ -49,9 +49,9 @@ class TestContentData < Test::Unit::TestCase
 
     assert_equal("8\nD12A1C98A3,765,2011/02/01 02:23:59.000\nB12A1C98A3,123123,2011/02/01 02:23:59.000\nD1234C98A3,12444,2011/02/01 02:23:59.000\nDB12A1C233,2,2011/02/01 02:23:59.000\nDB12A4338A,12412,2011/02/01 02:23:59.000\n232A1C98A3,124424,2011/02/01 02:23:59.000\nAC12A1C983,1242,2011/02/01 02:23:59.000\nAAC12A1C983,1242,2011/02/01 02:23:59.000\n3\nDB12A4338A,12412,large_server_2,dev1,/home/kuku/dev/lala/k.txt,2011/02/01 02:23:59.000\nD12A1C98A3,765,large_server_2,dev2,/home/lala/k.txt,2011/02/01 02:23:59.000\nDB12A4338A,12412,large_server_1,dev2,/home/kuku/lala/k.txt,2011/02/01 02:23:59.000\n",
       content_data.to_s)
-    content_data.to_file("content_data_test.data")
+    content_data.to_file("resources/content_data_test.data")
     new_content_data = ContentData.new()
-    new_content_data.from_file("content_data_test.data")
+    new_content_data.from_file("resources/content_data_test.data")
     assert_equal(new_content_data, content_data)
 
     content_data2 = ContentData.new
@@ -98,9 +98,9 @@ class TestContentData < Test::Unit::TestCase
     old_content_data.merge(content_data)
     assert_equal(true, old_content_data == content_data)
     content_data.merge(content_data2)
-    content_data.to_file("content_data_test2.data")
+    content_data.to_file("resources/content_data_test2.data")
     new_content_data2 = ContentData.new()
-    new_content_data2.from_file("content_data_test2.data")
+    new_content_data2.from_file("resources/content_data_test2.data")
     assert_equal(true, new_content_data2 == content_data)
     assert_equal(false, new_content_data2 == old_content_data)
 
