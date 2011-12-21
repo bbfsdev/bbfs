db0288ac7184b57ed68afa33084ae822b61beee6
genadyp <nukegluk@gmail.com>
2011-12-21 17:55:32 +0200

    path monitoring

---
+++ src/file_monitoring/monitor_path.rb
@@ -0,0 +1,258 @@
+
+
+
+
+class FileStatEnum
+  attr_reader :value
+  NEW = "NEW"
+  CHANGED = "CHANGED"
+  UNCHANGED = "UNCHANGED"
+  STABLE = "STABLE"
+
+  def self.contains? (value)
+    self.const_defined?(value, false)
+  end
+
+  def value= (new_value)
+    if FileStatEnum.contains?new_value
+      @value = new_value
+    else
+      raise ("Not an enum permitted value: #{new_value}")
+    end
+  end
+
+  def to_s
+    value
+  end
+end
+
+class FileStat
+  attr_accessor :scan_period, :stable_state, :state, :size, :modification_time
+  attr_reader :cycles, :path
+
+  DEFAULT_STABLE_STATE = 5
+
+  def initialize(path, size, modification_time, stable_state = DEFAULT_STABLE_STATE)
+    @path ||= path
+    @size = size
+    @modification_time = modification_time
+    @scan_period = scan_period
+    @stable_state = stable_state
+    @state = FileStatEnum::NEW
+    @cycles = 0
+  end
+
+  def monitor
+    file_stats = File.lstat(@path)
+    if (file_stats == nil or changed?)
+      @state = FileStatEnum::CHANGED
+    else
+      @cycles += 1
+      if @cycles >= @stable_state and (@state == FileStatEnum::UNCHANGED or @state == FileStatEnum::NEW)
+        @state = FileStatEnum::STABLE #if @state != FileStatEnum::STABLE
+      elsif @state != FileStatEnum::UNCHANGED
+        @state = FileStatEnum::UNCHANGED
+        @cycles = 0
+      end
+    end
+  end
+
+  def changed?
+    file_stat = File.lstat(@path)
+    if (file_stat == nil)
+      true
+    elsif (file_stat.size == size and file_stat.mtime.utc == modification_time.utc)
+      false
+    else
+      true
+    end
+  end
+
+  def state= (new_state)
+    if FileStatEnum.contains?new_state
+      @state = new_state
+    else
+      raise "Not a permitted state: #{new_state}"
+    end
+  end
+
+  def stable_state= (new_stable_state)
+    @stable_state = new_stable_state
+    if new_stable_state < @cycles_stable
+      state=(FileStatEnum::STABLE)
+      @cycles = 0
+    end
+  end
+
+  def == (other)
+    @path == other.path and @scan_period == other.scan_period and @stable_state == other.stable_state
+  end
+
+  def to_s (ident = 0)
+    (" " * ident) + path.to_s + " : " + state.to_s
+  end
+end
+
+class DirStat < FileStat
+  def initialize(path, size = nil, modification_time = nil, stable_state = DEFAULT_STABLE_STATE)
+    super
+    @dirs = Hash.new
+    @files = Hash.new
+  end
+  def add_dir (dir)
+    @dirs[dir.path] = dir
+  end
+
+  def add_file (file)
+    @files[file.path] = file
+  end
+
+  def rm_dir(dir)
+    @dirs.delete(dir.path)
+  end
+
+  def rm_file(file)
+    @files.delete(file.path)
+  end
+
+  def has_dir?(path)
+    @dirs.has_key?(path)
+  end
+
+  def has_file?(path)
+    @files.has_key?(path)
+  end
+
+  def get_dir(path)
+    if has_dir?path
+      @dirs[path]
+    else
+      raise "No such a dir: #{path}"
+    end
+  end
+
+  def get_file(path)
+    if has_dir?path
+      @files[path]
+    else
+      raise "No such a file: #{path}"
+    end
+  end
+
+  def get_child(path)
+    if has_file?path
+      return get_file(path)
+    elsif has_dir?path
+      return get_dir(path)
+    else
+      @dirs.each_value do |dir|
+        child = dir.get_child(path)
+        return child if child != nil
+      end
+    end
+    return nil
+  end
+
+  def changed?
+    # TODO
+    nil
+  end
+
+  def to_s(ident = 0)
+    ident_increment = 2
+    child_ident = ident + ident_increment
+    res = super
+    @files.each_value do |file|
+      res += "\n" + file.to_s(child_ident)
+    end
+    @dirs.each_value do |dir|
+      res += "\n" + dir.to_s(child_ident)
+    end
+    res
+  end
+
+  def monitor ()
+    files = Dir.glob(path + "/*")
+    is_init_monitor= false  # is this monitor run for DirState init ?
+    is_init_monitor = true if (@state == FileStatEnum::NEW and @cycles == 0 and @files.size == 0 and @dirs.size == 0)
+    was_changed = false
+
+    # monitor existing and absent files
+    @files.each_value do |file|
+      if files.include?(file.path) # existing file
+        file.monitor
+      else   # file was deleted
+             #TODO what we are doing in this case?
+        self.state=(FileStatEnum::CHANGED)
+        rm_file(file)
+        was_changed = true
+      end
+    end
+
+    @dirs.each_value do |dir|
+      if files.include?(dir.path)  # existing file
+        dir.monitor
+      else  #dir was deleted
+            #TODO what we are doing in this case?
+        self.state=(FileStatEnum::CHANGED)
+        rm_dir(dir)
+        was_changed = true
+      end
+    end
+
+    # add and monitor new files and directories
+    files.each do |file|
+      file_stat = File.lstat(file)
+      if (file_stat.directory?)
+        unless (has_dir?(file)) # new directory
+                                # change state only for existing directories
+                                # newly added directories have to remain with NEW state
+          unless is_init_monitor #if @state != FileStatEnum::NEW and @cycles != 0
+            self.state= FileStatEnum::CHANGED
+            was_changed = true
+          end
+          dir = DirStat.new(file, file_stat.size, file_stat.mtime.utc, self.stable_state)
+          add_dir(dir)
+          new_files = Dir.glob(dir.path + "/*")
+          new_files.each do |new_file|
+            new_file_stat = File.lstat(new_file)
+            if (new_file_stat.directory?)
+              new_dir = DirStat.new(new_file, new_file_stat.size, new_file_stat.mtime.utc, self.stable_state)
+              dir.add_dir(new_dir)
+              new_dir.monitor
+            else
+              dir.add_file(FileStat.new(new_file, new_file_stat.size, new_file_stat.mtime.utc, self.stable_state))
+            end
+          end
+        end
+      else # it is a file
+        unless(has_file?(file)) # new file
+                                # change state only for existing directories
+                                # newly added directories have to remain with NEW state
+          unless is_init_monitor #if @state != FileStatEnum::NEW and @cycles != 0
+            self.state= FileStatEnum::CHANGED
+            was_changed = true
+          end
+          add_file(FileStat.new(file, file_stat.size, file_stat.mtime, self.stable_state))
+        end
+      end
+    end
+
+    unless is_init_monitor or was_changed
+      @cycles += 1
+      if @cycles >= @stable_state and (@state == FileStatEnum::UNCHANGED or @state == FileStatEnum::NEW)
+        @state = FileStatEnum::STABLE #if @state != FileStatEnum::STABLE
+      else
+        @state = FileStatEnum::UNCHANGED
+        @cycles = 0
+      end
+    end
+  end
+
+end
+
+def main
+
+end
+
+main
---
+++ tests/file_monitoring/monitor_path_test.rb
@@ -0,0 +1,69 @@
+require './src/file_monitoring/monitor_path'
+require 'test/unit'
+require 'fileutils'
+
+class TestPathMonitor < Test::Unit::TestCase
+  # directory where tested files will be placed: <application_root_dir>/tests/../resources/time_modification_test
+  RESOURCES_DIR = File.expand_path(File.dirname(__FILE__) + "/../../resources/path_monitor_test")
+  MOVE_DIR_SRC = RESOURCES_DIR + "/dir1000/dir1500"
+  MOVE_DIR_DEST = RESOURCES_DIR
+  MOVE_FILE_SRC = RESOURCES_DIR + "/dir1000/test_file.1000"
+  MOVE_FILE_DEST = RESOURCES_DIR
+
+  def setup
+    sizes = [500, 1000, 1500]
+    numb_of_copies = 2
+    test_file_name = "test_file"   # file name format: <test_file_name_prefix>.<size>[.serial_number_if_more_then_1]
+
+    FileUtils.rm_rf(RESOURCES_DIR) if (File.exists?(RESOURCES_DIR))
+
+    # prepare files for testing
+    cur_dir = nil; # dir where currently files created in this iteration will be placed
+
+    sizes.each do |size|
+      file_name = test_file_name + "." + size.to_s
+
+      if (cur_dir == nil)
+        cur_dir = String.new(RESOURCES_DIR)
+      else
+        cur_dir = cur_dir + "/dir" + size.to_s
+      end
+      Dir.mkdir(cur_dir) unless (File.exists?(cur_dir))
+      raise "Can't create writable working directory: #{cur_dir}" unless (File.exists?(cur_dir) and File.writable?(cur_dir))
+
+      file_path = cur_dir + "/" + file_name
+      File.open(file_path, "w") do |file|
+        content = Array.new
+        size.times do |i|
+          content.push(sprintf("%5d ", i))
+        end
+        file.puts(content)
+      end
+
+      numb_of_copies.times do |i|
+        FileUtils.cp(file_path, "#{file_path}.#{i}")
+      end
+    end
+  end
+
+  def test_monitor
+    dir_stat = File.lstat(RESOURCES_DIR)
+    test_dir = DirStat.new(RESOURCES_DIR, dir_stat.size, dir_stat.mtime.utc)
+    test_dir.monitor
+    puts test_dir.to_s
+    test_dir.monitor
+    puts test_dir.to_s
+    FileUtils.mv MOVE_DIR_SRC, MOVE_DIR_DEST
+    test_dir.monitor
+    puts test_dir.to_s
+    test_dir.monitor
+    puts test_dir.to_s
+    FileUtils.mv MOVE_FILE_SRC, MOVE_FILE_DEST
+    test_dir.monitor
+    puts test_dir.to_s
+    test_dir.monitor
+    puts test_dir.to_s
+    test_dir.monitor
+    puts test_dir.to_s
+  end
+end
