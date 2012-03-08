6b15fd34d7646e96f418516df807a8dbee7094b2
genadyp <nukegluk@gmail.com>
2012-03-08 17:59:41 +0200

    project structure change

--- run_all_tests.rb
+++ run_all_tests.rb
@@ -3,10 +3,6 @@ require 'test/unit'
 
 @@root_dir = File.dirname(File.expand_path(__FILE__))
 
-Dir["#{@@root_dir}/tests/*"].each do |testcase|
-  require testcase
-end
-
 Dir["#{@@root_dir}/test/*"].each do |testcase|
   require testcase
 end
