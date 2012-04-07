8bb77964a9f25d623bceb2df86197614536b1944
kolmanv <kolmanv@gmail.com>
2012-04-07 09:56:01 +0300

    Updated copy.rb. Added test. Copied net_utils to copy.rb

--- lib/file_copy/copy.rb
+++ lib/file_copy/copy.rb
@@ -1,40 +1,43 @@
-require './configuration.rb'
-require './content_data.rb'
-require './net_utils.rb'
 require 'net/ssh'
 require 'net/sftp'
 
-class Copy
-  def initialize(configuration, content_data, dest_server, dest_path)
-    server_conf = configuration.find_server(dest_server)
+module BBFS
+  module FileCopy
 
-    ssh = connect(server_conf)
-    ssh.sftp.connect do |sftp|
-      arr = []
-      content_data.instances.values.each { |instance|
-        if File.exist?(instance.full_path)
-          arr << instance.full_path
-        else
-          p "File #{instance.full_path} does not exists localy."
-        end
-      }
+    # Creates ssh connection, assumes username and password
+    # or without password but with ssh keys.
+    def self.ssh_connect(username, password, server)
+      username = (username and username.length > 0) ? username : ENV['USER']
+      password = (password and password.length > 0) ? password : nil
+      port = 22 # 22 is a standart SSH port
+      raise "Undefined server" unless server
+      if (username and password)
+        Net::SSH.start(server, username,
+                       :password => password,
+                       :port => port)
+      elsif (username)
+        Net::SSH.start(server, username,
+                       :port => port)
+      else
+        raise "Undefined username"
+      end
+    end
 
-      uploads = arr.map { |f|
-        dest_base_filename = File.basename(f)
-        file_exists = true
-        while(file_exists) do
-          file_exists = false
-          sftp.dir.glob(dest_path, dest_base_filename) do |entry|
-            dest_base_filename = dest_base_filename + "_"
-            p "File exists at #{dest_path}/#{entry.name} remaning to #{dest_base_filename}"
-            file_exists = true
-          end
-        end
-        p "Copying #{f} to #{dest_server}:#{dest_path}/#{dest_base_filename}"
-        sftp.upload(f, "%s/%s" % [dest_path, dest_base_filename])
-      } # arr.map
-      uploads.each { |u| u.wait }
-      p "Done."
-    end # sftp.connect
-  end # def initialize
-end # class Copy
+    # Simply copy map files using sftp server on dest_server.
+    # Note: stfp.upload support parallel uploads - default is 2.
+    def self.sftp_copy(username, password, server, files_map)
+      ssh = FileCopy.ssh_connect(username, password, server)
+      ssh.sftp.connect do |sftp|
+        uploads = files_map.map { |from,to|
+          # TODO(kolman): Create destination directory is not exists.
+          p "Copying #{from} to #{to}"
+          sftp.upload(from, to)
+        }
+        uploads.each { |u| u.wait }
+        p "Done."
+      end # sftp.connect
+    end # def initialize
+
+  end
+end
+
---
+++ test/file_copy/copy_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../../lib/file_copy/copy.rb'
+
+module BBFS
+  module FileCopy
+    module Spec
+
+      describe 'FileCopy::ssh_connect' do
+        it 'should raise error when username not specified' do
+          ENV.should_receive(:[]).with("USER").and_return(nil)
+          expect { FileCopy::ssh_connect(nil, nil, 'a server') }.should raise_error "Undefined username"
+        end
+
+        it 'should raise error when server not specified' do
+          expect { FileCopy::ssh_connect('kuku', nil, nil) }.should raise_error "Undefined server"
+        end
+
+        it 'should try to connect if username is set explicitly' do
+          Net::SSH.should_receive(:start).with(any_args()).and_return(true)
+          FileCopy::ssh_connect('kuku', nil, 'a server').should == true
+        end
+        it 'should try to connect if username is set via ENV variable' do
+          Net::SSH.should_receive(:start).with(any_args()).and_return(true)
+          ENV.should_receive(:[]).with("USER").and_return('kuku')
+          FileCopy::ssh_connect(nil, nil, 'a server').should == true
+        end
+      end
+
+      # TODO(kolman): Very bad test, should rewrite and understand how to
+      # write test correctly.
+      describe 'FileCopy::sftp_copy' do
+        it 'should copy files' do
+          ssh_connection = double('Net::SSH::Connection')
+          FileCopy.should_receive(:ssh_connect).with(any_args()).and_return(ssh_connection)
+          sftp_session = double('Net::SFTP::Session')
+          ssh_connection.should_receive(:sftp).with(any_args()).and_return(sftp_session)
+          sftp_session.should_receive(:connect).with(any_args()).and_yield(sftp_session)
+          uploader = double('Operations::Upload')
+          uploader.should_receive(:wait).with(any_args()).and_return(true, true)
+          sftp_session.should_receive(:upload).with('a', 'b').and_return(uploader)
+          sftp_session.should_receive(:upload).with('c', 'd').and_return(uploader)
+          FileCopy::sftp_copy(nil, nil, nil, { 'a' => 'b', 'c' => 'd' })
+        end
+      end
+
+    end
+  end
+end
