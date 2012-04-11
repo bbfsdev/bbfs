37fe0e063ac82e8a6e797f9b98e515d32cbc7ee9
kolmanv <kolmanv@gmail.com>
2012-04-11 16:32:46 +0300

    added log path parameter + changed " => '

--- lib/file_monitoring/file_monitoring.rb
+++ lib/file_monitoring/file_monitoring.rb
@@ -1,11 +1,15 @@
 require 'algorithms'
 require 'fileutils'
+require 'params'
 require 'yaml'
 
 require_relative 'monitor_path'
 
 module BBFS
   module FileMonitoring
+
+    PARAMS.parameter('default_log_path', File.expand_path('~/.bbfs/log/file_monitoring.log'),
+                     'Default path for log file.')
 
     class FileMonitoring
 
@@ -20,30 +24,30 @@ module BBFS
       def monitor_files
         config_yml = YAML::load_file(@config_path)
 
-        conf_array = config_yml["paths"]
+        conf_array = config_yml['paths']
 
         pq = Containers::PriorityQueue.new
         conf_array.each { |elem|
-          priority = (Time.now + elem["scan_period"]).to_i
-          dir_stat = DirStat.new(elem["path"], elem["stable_state"])
+          priority = (Time.now + elem['scan_period']).to_i
+          dir_stat = DirStat.new(elem['path'], elem['stable_state'])
           dir_stat.set_event_queue(@event_queue) if @event_queue
           pq.push([priority, elem, dir_stat], -priority)
         }
 
-        log_path = File.expand_path("~/.bbfs/log/file_monitoring.log")
-        if config_yml.key?("log_path")
-          log_path = File.expand_path(config_yml["log_path"])
+        log_path = PARAMS.default_log_path
+        if config_yml.key?('log_path')
+          log_path = File.expand_path(config_yml['log_path'])
         end
 
-        puts "Log path:" + log_path
+        puts 'Log path:' + log_path
         FileUtils.mkdir_p(File.dirname(log_path))
         log = File.open(log_path, 'w')
         FileStat.set_log(log)
 
         while true do
           time, conf, dir_stat = pq.pop
-          #puts "time:" + time.to_s()
-          #puts "now:" + Time.now.to_i.to_s()
+          #puts 'time:' + time.to_s()
+          #puts 'now:' + Time.now.to_i.to_s()
           #puts conf
 
           time_span = time - Time.now.to_i
@@ -53,9 +57,9 @@ module BBFS
 
           dir_stat.monitor
 
-          #puts conf["path"]
-          #puts conf["scan_period"]
-          priority = (Time.now + conf["scan_period"]).to_i
+          #puts conf['path']
+          #puts conf['scan_period']
+          priority = (Time.now + conf['scan_period']).to_i
           pq.push([priority, conf, dir_stat], -priority)
         end
 
