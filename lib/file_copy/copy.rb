a014c2c8a37f6c5e03f5fa321295e31de1f73e97
kolmanv <kolmanv@gmail.com>
2012-04-11 16:10:59 +0300

    Added recursive mkdir.

--- lib/file_copy/copy.rb
+++ lib/file_copy/copy.rb
@@ -6,11 +6,12 @@ module BBFS
 
     # Creates ssh connection, assumes username and password
     # or without password but with ssh keys.
-    def self.ssh_connect(username, password, server)
+    def ssh_connect(username, password, server)
       username = (username and username.length > 0) ? username : ENV['USER']
       password = (password and password.length > 0) ? password : nil
       port = 22 # 22 is a standart SSH port
       raise "Undefined server" unless server
+      p "Trying to connect(ssh): #{username}, #{password}, #{server}."
       if (username and password)
         Net::SSH.start(server, username,
                        :password => password,
@@ -22,22 +23,43 @@ module BBFS
         raise "Undefined username"
       end
     end
+    module_function :ssh_connect
 
     # Simply copy map files using sftp server on dest_server.
     # Note: stfp.upload support parallel uploads - default is 2.
     # TODO(kolman): packing files, all, not all, determine by part of file.
-    def self.sftp_copy(username, password, server, files_map)
+    def sftp_copy(username, password, server, files_map)
       ssh = FileCopy.ssh_connect(username, password, server)
       ssh.sftp.connect do |sftp|
         uploads = files_map.map { |from,to|
-          # TODO(kolman): Create destination directory is not exists.
+          remote_dir = File.dirname(to)
+          sftp_mkdir_recursive(sftp, File.dirname(to))
           p "Copying #{from} to #{to}"
           sftp.upload(from, to)
         }
         uploads.each { |u| u.wait }
         p "Done."
-      end # sftp.connect
+      end # ssh.sftp.connect
     end # def initialize
+    module_function :sftp_copy
+
+    def sftp_mkdir_recursive(sftp, path)
+      dir_stat = nil
+      begin
+        p "Stat remote dir: #{path}."
+        dir_stat = sftp.stat!(path).directory?
+        p "Stat result #{dir_stat}."
+      rescue Net::SFTP::StatusException
+      end
+      if !dir_stat
+        p "Directory does not exists: #{path}."
+        sftp_mkdir_recursive sftp, File.dirname(path)
+        sftp.mkdir!(path)
+      end
+    end
+    module_function :sftp_mkdir_recursive
+
+
 
   end
 end
