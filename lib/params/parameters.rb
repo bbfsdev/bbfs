447154554d560bae57a0c8d67ea385bb691f479f
Alexey N <alexeyn66@gmail.com>
2012-04-21 08:32:33 +0300

    Adding parameters.rb

---
+++ lib/params/parameters.rb
@@ -0,0 +1,64 @@
+require 'optparse'
+
+module BBFS
+  module Params
+
+#Interface (not final):
+#add_flag(command_name, flag_name, flag_type,
+# flag_default_value, flag_description)  // adds flag configuration to
+#                                           argument parser module.
+
+#get_value(flag_name)  // returns the value of flag_name,
+#                        if values was not provided return the default value,
+#                        if no default value, output error.
+
+#get_command()  // returns the command
+#                (first string in the argv array)
+
+    class Parameters
+
+      def initialize
+        @m_flags = Hash.new #define flags hash
+        @flags_values = Hash.new #define flags values hash
+      end
+
+      def add_flag(command_name, flag_name, flag_type,
+          flag_default_value, flag_description )
+
+        if !@m_flags.has_key?(command_name)
+          @m_flags[command_name] = Hash.new            # store required command
+        end
+
+        if !@m_flags[command_name].has_key?(flag_name)
+          @m_flags[command_name][flag_name] = Hash.new            # store values for the type
+        end
+
+        @m_flags[command_name][flag_name]['flag_type'] = flag_type  # store flag type
+        @m_flags[command_name][flag_name]['flag_value'] = flag_default_value  # store default value
+        @m_flags[command_name][flag_name]['flag_description'] = flag_description  # store flag description
+
+        return true
+      end
+
+
+      #This function returns all stored keys
+      def get_command()
+        return @m_flags.keys
+        return true
+      end
+
+      #This function returns a value for given
+      #command and flag name
+      def get_flag_value(command, flag_name)
+        if @m_flags.has_key?(command)
+          if @m_flags[command].has_key?(flag_name)
+            return @m_flags[command][flag_name]['flag_value']
+          end
+        end
+        return false
+      end
+    end
+  end
+end
+
+
