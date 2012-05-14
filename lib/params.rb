9623137019e21a939400c0f61ea561123ae80bf7
yarondbb <yaron.dror.bb@gmail.com>
2012-05-15 01:19:10 +0300

    Changed yml foramt to be a simple hash of param=value Added check that If yml param does not exist in Params Module then the method will fail Added tests for the above behavior

--- lib/params.rb
+++ lib/params.rb
@@ -10,7 +10,14 @@ module BBFS
 
     def read_from_yml file_path
       proj_params = YAML::load_file(file_path)
-      proj_params.each {|param|  self.instance_variable_set '@' + param['name'], param['value']}
+      proj_params.keys.each do
+      |key|
+        if not self.instance_variable_get '@' + key
+           puts "loaded yml param:'#{key}' which does not exist in Params module."
+          return false
+        end
+        self.instance_variable_set '@' + key, proj_params[key]
+      end
     end
 
     module_function :parameter, :read_from_yml
--- test/params/params_test.rb
+++ test/params/params_test.rb
@@ -14,23 +14,28 @@ module BBFS
     def test_define_both_code_and_yml_params
       # Test set phase
       code_param_name =  'param_1'
-      code_param_value = 'value_defined_at_test_code'
-      code_param_desc =  'Defined at test code'
-      yml_param_value = 'value_defined_at_yml_file'
+      code_param_value = 'value_1_code'
+      yml_param_value = 'value_1_yml'
       yml_file_name = 'test/params/test_1.yml'
       #define param in the code
-      Params.parameter code_param_name, code_param_value , code_param_desc
+      Params.parameter code_param_name, code_param_value , 'Defined at test code'
 
       # Test check phase
       # First check the param_1 was defined correctly at Params
       assert_equal code_param_value, Params.param_1 , 'Abnormal behaviour. "Params" module does not work'
       #read params from yml file. This should override the param value
       Params.read_from_yml(File.expand_path(yml_file_name))
-      # Now check that param_1 value was overriden by the yml file params
+      # Now check that param_1 value was overridden by the yml file params
       assert_equal yml_param_value, Params.param_1 , "parameter:[#{code_param_name}] " + \
-      "was defined both at the test code with value:[#{code_param_value}], " + \
-      "and at the yml file:#{yml_file_name} with value:[#{yml_param_value}]. " \
-      "The test expected the yml file value to overwrite the code value but it did not."
+        "was defined both at the test code with value:[#{code_param_value}], " + \
+        "and at the yml file:#{yml_file_name} with value:[#{yml_param_value}]. " \
+        "The test expected the yml file value to overwrite the code value but it did not."
+    end
+
+    def test_yml_loads_an_undefined_param
+      yml_file_name = 'test/params/test_2.yml'
+      assert_equal false, Params.read_from_yml(File.expand_path(yml_file_name)) , \
+        'Error. yml param should not have been defined in Params'
     end
   end
 end
