e5a7bf8fc11d96c1974fbc6d983eacb18ab052d5
yarondbb <yaron.dror.bb@gmail.com>
2012-05-15 23:24:54 +0300

    Removed test files. Using yml string instead read_yml_params will return true if all parameters loaded successfully. False otherwise.

--- lib/params.rb
+++ lib/params.rb
@@ -3,24 +3,25 @@ require 'yaml'
 # A simple params module.
 module BBFS
   module Params
-    def parameter(name, default_value, description)
+    def Params.parameter(name, default_value, description)
       self.instance_variable_set '@' + name, default_value
       self.class.send('attr_accessor', name)
     end
 
-    def read_from_yml file_path
-      proj_params = YAML::load_file(file_path)
-      proj_params.keys.each do
-      |key|
-        if not self.instance_variable_get '@' + key
-           puts "loaded yml param:'#{key}' which does not exist in Params module."
+    # Load yml params and override default values.
+    # Return true, if all yml params loaded successfully.
+    # Return false, if a loaded yml param does not exist.
+    def Params.read_yml_params yml_params_string
+      proj_params = YAML::load(yml_params_string)
+      proj_params.keys.each do |param_name|
+        if not self.instance_variable_get '@' + param_name
+          puts "loaded yml param:'#{param_name}' which does not exist in Params module."
           return false
         end
-        self.instance_variable_set '@' + key, proj_params[key]
+        self.instance_variable_set '@' + param_name, proj_params[param_name]}
       end
+      return true
     end
-
-    module_function :parameter, :read_from_yml
   end
 end
 
--- test/params/params_test.rb
+++ test/params/params_test.rb
@@ -10,31 +10,29 @@ require_relative '../../lib/params.rb'
 module BBFS
 
   class TestLog < Test::Unit::TestCase
-
-    def test_define_both_code_and_yml_params
+    def test_override_predefined_param_with_loaded_yml_param
       # Test set phase
       code_param_name =  'param_1'
       code_param_value = 'value_1_code'
+      yml_string = 'param_1: value_1_yml'
       yml_param_value = 'value_1_yml'
-      yml_file_name = 'test/params/test_1.yml'
-      #define param in the code
+      #define a new param
       Params.parameter code_param_name, code_param_value , 'Defined at test code'
 
       # Test check phase
-      # First check the param_1 was defined correctly at Params
+      # 1. Check that param_1 was defined correctly at Params
       assert_equal code_param_value, Params.param_1 , 'Abnormal behaviour. "Params" module does not work'
       #read params from yml file. This should override the param value
-      Params.read_from_yml(File.expand_path(yml_file_name))
-      # Now check that param_1 value was overridden by the yml file params
+      Params.read_yml_params(yml_string)
+      # 2. Check that param_1 value was overridden by the yml param
       assert_equal yml_param_value, Params.param_1 , "parameter:[#{code_param_name}] " + \
         "was defined both at the test code with value:[#{code_param_value}], " + \
-        "and at the yml file:#{yml_file_name} with value:[#{yml_param_value}]. " \
+        "and at the yml string [#{yml_string}] with value:[#{yml_param_value}]. " \
         "The test expected the yml file value to overwrite the code value but it did not."
     end
 
     def test_yml_loads_an_undefined_param
-      yml_file_name = 'test/params/test_2.yml'
-      assert_equal false, Params.read_from_yml(File.expand_path(yml_file_name)) , \
+      assert_equal false, Params.read_yml_params('param_2: value_2_yml') , \
         'Error. yml param should not have been defined in Params'
     end
   end
