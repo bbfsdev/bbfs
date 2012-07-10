9ea7c6e688cf42d98cc506dd9058e5e5f613fe23
kolmanv <kolmanv@gmail.com>
2012-07-10 07:40:12 +0300

    Started writing code.

---
+++ lib/networking.rb
@@ -0,0 +1,3 @@
+class Networking
+  # To change this template use File | Settings | File Templates.
+end
---
+++ lib/networking/persistent_queue.rb
@@ -0,0 +1,3 @@
+class PersistentQueue
+  # To change this template use File | Settings | File Templates.
+end
