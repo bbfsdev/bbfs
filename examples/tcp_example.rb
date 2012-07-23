f03254aef2d42193cb162f7c29d8c22c833c30ca
kolmanv <kolmanv@gmail.com>
2012-07-23 06:18:54 +0300

    Fixes + logging.

--- examples/tcp_example.rb
+++ examples/tcp_example.rb
@@ -2,31 +2,35 @@ require 'log'
 require 'networking/tcp'
 
 BBFS::Params.log_write_to_console = 'true'
-BBFS::Params.log_debug_level = 3
+BBFS::Params.log_debug_level = 1
+BBFS::Params.log_param_number_of_mega_bytes_stored_before_flush = 0
+BBFS::Params.log_param_max_elapsed_time_in_seconds_from_last_flush = 0
+
 BBFS::Log.init
 
-def server_receive_obj(hello_msg)
-  puts hello_msg
+def server_receive_obj(addr_info, hello_msg)
+  BBFS::Log.debug1(hello_msg)
+  BBFS::Log.debug1(addr_info.inspect)
 end
 
 def client_receive_obj(hello_msg)
-  puts hello_msg
-  $tcp_client.send_obj('Hello back.')
+  BBFS::Log.debug1(hello_msg)
+  $tcp_client.send_obj('Hello again.')
 end
 
-puts 'Creating TCP server and client.'
+BBFS::Log.info('Creating TCP client and server.')
 $tcp_server = BBFS::Networking::TCPServer.new(5555, method(:server_receive_obj))
+#sleep 1
 $tcp_client = BBFS::Networking::TCPClient.new('localhost', 5555, method(:client_receive_obj))
+#sleep 0.1
+#BBFS::Log.info('Wait for the connection to be established.')
 
-puts 'Waiting 1 sec for connection.'
-# Wait for the connection to be established
-sleep 1
+BBFS::Log.info('Sending hello...')
+BBFS::Log.info("send_obj:#{$tcp_client.send_obj('Hello from client.')}")
+#$tcp_server.send_obj('Hello from server.')
 
-puts 'Sending hello...'
-$tcp_client.send_obj('Hello clients.')
-
-puts 'Waiting on threads.'
+BBFS::Log.info('Waiting on threads.')
 # Wait for all threads (don't exit).
-$tcp_client.tcp_thread.join
 $tcp_server.tcp_thread.join
-puts 'Should not get here.'
+$tcp_client.tcp_thread.join
+BBFS::Log.info('Should not get here.')
--- lib/networking/tcp.rb
+++ lib/networking/tcp.rb
@@ -5,12 +5,12 @@ module BBFS
   module Networking
 
     def Networking.write_to_stream(stream, obj)
-      Log.debug1('Writing to stream.')
+      Log.debug3('Writing to stream.')
       marshal_data = Marshal.dump(obj)
-      Log.debug1("Writing data size: #{marshal_data.length}")
+      Log.debug3("Writing data size: #{marshal_data.length}")
       data_size = [marshal_data.length].pack("l")
       if data_size.nil? || marshal_data.nil?
-        Log.debug1 'Send data size is nil!'
+        Log.debug3 'Send data size is nil!'
       end
       bytes_written = stream.write data_size
       bytes_written += stream.write marshal_data
@@ -19,13 +19,13 @@ module BBFS
 
     # Returns pair [status(true/false), obj]
     def Networking.read_from_stream(stream)
-      Log.debug1('Read from stream.')
+      Log.debug3('Read from stream.')
       return [false, nil] unless size_of_data = stream.read(4)
       size_of_data = size_of_data.unpack("l")[0]
-      Log.debug1("size_of_data:#{size_of_data}")
+      Log.debug3("size_of_data:#{size_of_data}")
       data = stream.read(size_of_data)
       unmarshalled_data = Marshal.load(data)
-      Log.debug1("unmarshalled_data:#{unmarshalled_data}")
+      Log.debug3("unmarshalled_data:#{unmarshalled_data}")
       return [true, unmarshalled_data]
     end
 
@@ -43,10 +43,11 @@ module BBFS
         @max_parallel = max_parallel
         @sockets = {}
         @tcp_thread = run_server
+        @tcp_thread.abort_on_exception = true
       end
 
       def send_obj(obj, addr_info=nil)
-        Log.debug1("addr_info=#{addr_info}")
+        Log.debug3("addr_info=#{addr_info}")
         if addr_info
           Networking.write_to_stream(@sockets[addr_info], obj) > 0
         else
@@ -59,19 +60,20 @@ module BBFS
       private
       # Creates new thread/pool
       def run_server
-        Log.debug1('run_server')
+        Log.debug3('run_server')
         return Thread.new do
-          Log.debug1('run_server2')
+          Log.debug3('run_server2')
           Socket.tcp_server_loop(@port) do |sock, addr_info|
-            Log.debug1('tcp_server_loop ' + sock.string + ' ' + addr_info)
+            Log.debug3('-----')
+            Log.debug3('tcp_server_loop... %s' % addr_info.inspect)
             @sockets[addr_info] = sock
-            Log.debug1('tcp_server_loop')
+            Log.debug3('tcp_server_loop')
             @new_clb.call(addr_info) if @new_clb
             loop do
               # Blocking read.
-              Log.debug1('read_from_stream')
+              Log.debug3('read_from_stream')
               status, obj = Networking.read_from_stream(sock)
-              Log.debug1("Returned from read: #{status}, #{obj}")
+              Log.debug3("Returned from read: #{status}, #{obj}")
               @obj_clb.call(addr_info, obj) if @obj_clb && status
               break unless status
             end
@@ -91,28 +93,37 @@ module BBFS
         @obj_clb = obj_clb
         @reconnected_clb = reconnected_clb
         Log.debug1('TCPClient init.')
-        @tcp_thread = start_reading if @obj_clb
+        Log.debug1("TCPClient init...#{@obj_clb}...")
+        if @obj_clb
+          @tcp_thread = start_reading
+          @tcp_thread.abort_on_exception = true
+        end
       end
 
       def send_obj(obj)
+        Log.debug1('send_obj')
         open_socket unless socket_good?
+        Log.debug1('after open')
         if !socket_good?
           Log.warning('Socket not opened for writing, skipping send.')
           return false
         end
+        Log.debug1('writing...')
         bytes_written = Networking.write_to_stream(@tcp_socket, obj)
         return bytes_written > 0
       end
 
       private
       def open_socket
-        Log.debug1 "Connecting to content server #{@host}:#{@port}."
+        Log.debug1("Connecting to content server #{@host}:#{@port}.")
         @tcp_socket = TCPSocket.new(@host, @port)
-        @reconnected_clb.call if @reconnected_clb && socket_good?
+        Log.debug1("Reconnect clb: '#{@reconnected_clb}'")
+        #@reconnected_clb.call if @reconnected_clb && socket_good?
       end
 
       private
       def socket_good?
+        Log.debug1 "socket_good? #{@tcp_socket && !@tcp_socket.closed?}"
         return @tcp_socket && !@tcp_socket.closed?
       end
 
@@ -121,9 +132,13 @@ module BBFS
         Log.debug1('start_reading (TCPClient).')
         return Thread.new do
           loop do
-            Log.debug1('Start blocking read (TCPClient).')
+            Log.debug3('Start blocking read (TCPClient).')
             # Blocking read.
             open_socket unless socket_good?
+            if !socket_good?
+              Log.warning('Socket not closed, breaking client reading thread.')
+              break
+            end
             status, obj = Networking.read_from_stream(@tcp_socket)
             # Handle case when socket is closed in middle.
             # In that case we should not call obj_clb.
