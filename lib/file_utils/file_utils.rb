d77666520c2f5c77593c37ebf518a644b0a26989
AlexeyNemytov <alexeyn66@gmail.com>
2012-08-10 10:43:03 +0300

    1. added paranthesis to all Log methods calls and all methods in file which have one or more parameters
    2. changed info to warning or error to all cases
    
    3.removed resundant space line135

--- lib/file_utils/file_utils.rb
+++ lib/file_utils/file_utils.rb
@@ -32,29 +32,29 @@ module BBFS
       def FileUtils.run
         if Params['command'] == 'mksymlink'
           if Params['ref_cd'].nil?
-            Log.info "--ref_cd is not set"
+            Log.error ("--ref_cd is not set")
             return
           end
           ref_cd = ContentData.new()
           ref_cd.from_file(Params['ref_cd'])
           if ref_cd.nil?
-            Log.info "Error loading content data ref_cd=%s" % Params['ref_cd']
+            Log.error ("Error loading content data ref_cd=%s" % Params['ref_cd'])
             return
           end
 
           if Params['base_cd'].nil?
-            Log.info "--base_cd is not set"
+            Log.error ("--base_cd is not set")
             return
           end
           base_cd = ContentData.new()
           base_cd.from_file(Params['base_cd'])
           if base_cd.nil?
-            Log.info "Error loading content data base_cd=%s" % Params['base_cd']
+            Log.error ("Error loading content data base_cd=%s" % Params['base_cd'])
             return
           end
 
           if Params['dest'].nil?
-            Log.info "--dest is not set"
+            Log.error ("--dest is not set")
             return
           end
 
@@ -62,7 +62,7 @@ module BBFS
           begin
             not_found = FileUtil.mksymlink(ref_cd, base_cd, Params['dest'])
           rescue NotImplementedError
-            Log.info "symlinks are unimplemented on this machine"
+            Log.error ("symlinks are unimplemented on this machine")
             return nil
           end
           return not_found
@@ -71,29 +71,29 @@ module BBFS
             Params['command'] == "minus")
 
           if Params['cd_a'].nil?
-            Log.info "--cd_a is not set"
+            Log.error ("--cd_a is not set")
             return
           end
           cd_a = ContentData.new()
           cd_a.from_file(Params['cd_a'])
           if cd_a.nil?
-            Log.info "Error loading content data cd_a=%s" % Params['cd_a']
+            Log.error ("Error loading content data cd_a=%s" % Params['cd_a'])
             return
           end
 
           if Params['cd_b'].nil?
-            Log.info "--cd_b is not set"
+            Log.error ("--cd_b is not set")
             return
           end
           cd_b = ContentData.new()
           cd_b.from_file(Params['cd_b'])
           if cd_b.nil?
-            Log.info "Error loading content data cd_b=%s" % Params['cd_b']
+            Log.error ("Error loading content data cd_b=%s" % Params['cd_b'])
             return
           end
 
           if Params['cd_b'].nil?
-            Log.info "--dest is not set"
+            Log.error ("--dest is not set")
             return
           end
 
@@ -101,20 +101,20 @@ module BBFS
 
         elsif Params['command'] == 'unify_time'
           if  Params['cd'].nil?
-            Log.info "--cd is not set"
+            Log.error ("--cd is not set")
             return
           end
           cd = ContentData.new()
           cd.from_file(Params['cd'])
           if cd.nil?
-            Log.info "Error loading content data cd=%s" % Params['cd']
+            Log.error ("Error loading content data cd=%s" % Params['cd'])
             return
           end
           output = unify_time(cd)
           # indexer
         elsif Params['command'] == 'indexer'
           if Params['patterns'].nil?
-            Log.info "--patterns is not set"
+            Log.error ("--patterns is not set")
             return
           end
 
@@ -125,7 +125,7 @@ module BBFS
           }
 
           unless patterns.size > 0
-            Log.info "Error loading patterns=%s (empty file)" % Params['patterns']
+            Log.error ("Error loading patterns=%s (empty file)" % Params['patterns'])
             return
           end
 
@@ -133,8 +133,8 @@ module BBFS
           if not Params['exist_cd'].nil?
             exist_cd = BBFS::ContentData::ContentData.new()
             exist_cd.from_file(Params['exist_cd'])
-            if  exist_cd.nil?
-              Log.info "Error loading content data exist_cd=%s" % Params['exist_cd']
+            if exist_cd.nil?
+              Log.error ("Error loading content data exist_cd=%s" % Params['exist_cd'])
               return
             end
           end
@@ -144,11 +144,11 @@ module BBFS
           # crawler
         elsif Params['command'] == 'crawler'
           if Params['conf_file'].nil?
-            Log.info "--conf_file is not set"
+            Log.error ("--conf_file is not set")
             return
           end
           if not File.exists?(Params['conf_file'])
-            Log.info "config file doesn't exist conf_file=%s" % Params['conf_file']
+            Log.error ("config file doesn't exist conf_file=%s" % Params['conf_file'])
             return
           end
 
@@ -158,7 +158,7 @@ module BBFS
           end
           unless (Params['cd_in'].nil?)
             if not File.exists?(Params['conf_file'])
-              Log.info "input data file doesn't exist cd_in=%s" % Params['cd_in']
+              Log.error ("input data file doesn't exist cd_in=%s" % Params['cd_in'])
               return
             end
           end
@@ -216,7 +216,7 @@ module BBFS
             if (file_mtime == db.instances[instance.global_path].modification_time \
                 and file_size == instance.size \
                 and (file_mtime <=> instance.modification_time) > 0)
-              Log.info "Comparison success."
+              Log.info ("Comparison success.")
               File.utime File.atime(instance.full_path), instance.modification_time, instance.full_path
               file_mtime = File.open(instance.full_path) { |f| f.mtime }
               Log.info "file mtime:#{file_mtime}."
@@ -257,7 +257,7 @@ module BBFS
             warnings << "Warning: base content does not contains:'%s'" % instance.checksum
           end
         }
-        Log.info warnings
+        Log.warning (warnings)
         return not_found
       end
     end
