b9bce388802725ba9da4b5ccd6363a0ab532fefb
yarondbb <yaron.dror.bb@gmail.com>
2013-03-29 00:29:44 +0200

    1. Fixed file init. File is deleted if eists. Dir created unless exists
    2. Enabled email.
           Each error or fatal will be flushed imidiately to email.
           Added Log.flush method to allow manual flush of all outputters (including email) by the user.

--- lib/log.rb
+++ lib/log.rb
@@ -24,10 +24,12 @@ module Log
   Params.integer('log_debug_level', 0 , 'Log level.')
   Params.boolean('log_write_to_file', true , \
       'If true then the logger will write the messages to a file.')
+  Params.path('log_file_name', "~/.bbfs/log/#{Log.executable_name}.log4r" , \
+      'Default log file name: ~/.bbfs/log/<executable_name>.log')
   Params.boolean('log_write_to_console', false , \
       'If true then the logger will write the messages to the console.')
-  Params.path('log_file_name', File.expand_path("~/.bbfs/log/#{Log.executable_name}.log") , \
-      'Default log file name: ~/.bbfs/log/<executable_name>.log')
+  Params.boolean('log_write_to_email', false , \
+      'If true then the logger will write the error and fatal messages to email.')
   Params.string('from_email', 'bbfsdev@gmail.com', 'From gmail address for update.')
   Params.string('from_email_password', 'Only2Gether', 'From gmail password.')
   Params.string('to_email', 'bbfsdev@gmail.com', 'Destination email for updates.')
@@ -36,41 +38,56 @@ module Log
     @log4r = Log4r::Logger.new 'BBFS log'
     @log4r.trace = true
 
-    #log4r levels
-    if 0 == Params['log_debug_level']
-      log4r_level = Log4r::INFO
-    else
-      log4r_level = Log4r::DEBUG
-    end
+    #levels setup
+    log4r_level = Log4r::DEBUG
+    log4r_level = Log4r::INFO if 0 == Params['log_debug_level']
 
+    #formatters
     formatter = Log4r::PatternFormatter.new(:pattern => "[%l] [%d] [%m]")
 
-    stdout_outputter = Log4r::Outputter.stdout
-    stdout_outputter.formatter = formatter
-    stdout_outputter.level = log4r_level
+    #stdout setup
+    if Params['log_write_to_console']
+      stdout_outputter = Log4r::Outputter.stdout
+      stdout_outputter.formatter = formatter
+      stdout_outputter.level = log4r_level
+      @log4r.outputters << stdout_outputter
+    end
 
-    file_outputter = Log4r::FileOutputter.new('file_log', :filename => "#{Params['log_file_name']}.log4r")
-    file_outputter.level = log4r_level
-    file_outputter.formatter = formatter
+    #file setup
+    if Params['log_write_to_file']
+      if File.exist?(Params['log_file_name'])
+        File.delete Params['log_file_name']
+      else
+        dir_name = File.dirname(Params['log_file_name'])
+        FileUtils.mkdir_p(dir_name) unless File.directory?(dir_name)
+      end
+      file_outputter = Log4r::FileOutputter.new('file_log', :filename => Params['log_file_name'])
+      file_outputter.level = log4r_level
+      file_outputter.formatter = formatter
+      @log4r.outputters << file_outputter
+    end
 
-    server_name = `hostname`.strip
-    email_outputter = Log4r::EmailOutputter.new('email_log',
-                                                :server => 'smtp.gmail.com',
-                                                :port => 587,
-                                                :subject => "Error happened in #{server_name} server run by #{ENV['USER']}.",
-                                                :acct => Params['from_email'],
-                                                :from => Params['from_email'],
-                                                :passwd => Params['from_email_password'],
-                                                :to => Params['to_email'],
-                                                :immediate_at => 'FATAL,ERROR',
-                                                :authtype => :plain,
-                                                :tls => true)
-    email_outputter.level = log4r_level
-    email_outputter.formatter = formatter
-
-    @log4r.outputters << stdout_outputter if Params['log_write_to_console']
-    @log4r.outputters << file_outputter if Params['log_write_to_file']
-    #@log4r.outputters << email_outputter
+    #email setup
+    if Params['log_write_to_file']
+      server_name = `hostname`.strip
+      email_outputter = Log4r::EmailOutputter.new('email_log',
+                                                  :server => 'smtp.gmail.com',
+                                                  :port => 587,
+                                                  :subject => "Error happened in #{server_name} server run by #{ENV['USER']}.",
+                                                  :acct => Params['from_email'],
+                                                  :from => Params['from_email'],
+                                                  :passwd => Params['from_email_password'],
+                                                  :to => Params['to_email'],
+                                                  :immediate_at => 'FATAL,ERROR',
+                                                  :authtype => :plain,
+                                                  :tls => true,
+                                                  :formatfirst => true,
+                                                  :buffsize => 9999,
+      )
+      email_outputter.level = Log4r::ERROR
+      email_outputter.formatter = formatter
+      @log4r.outputters << email_outputter
+    end
 
     # Write init message and user parameters
     @log4r.info 'BBFS Log initialized.'  # log first data
@@ -112,6 +129,11 @@ module Log
   # Log debug level 3 massages
   def Log.debug3(msg)
     @log4r.debug(msg_with_caller(msg))
+  end
+
+  # Flush email log
+  def Log.flush()
+    @log4r.outputters.each_outputter {|o| o.flush}
   end
 
   private_class_method(:msg_with_caller)
