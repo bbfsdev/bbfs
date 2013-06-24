8c2d7fd93427a57824c961e0551b128605a9fe48
yarondbb <yaron.dror.bb@gmail.com>
2013-06-24 20:20:49 +0300

    Updated gems versions to 1.1.0 and gems scripts

--- gem_build_lnx.bat
+++ gem_build_lnx.bat
@@ -26,16 +26,16 @@ call gem build validations.gemspec
 echo -------------------------------------
 echo Start installing content server gems:
 echo -------------------------------------
-call gem install log-1.0.1.gem
-call gem install content_data-1.0.1.gem
-call gem install email-1.0.1.gem
-call gem install file_copy-1.0.1.gem
-call gem install file_indexing-1.0.1.gem
-call gem install file_monitoring-1.0.1.gem
-call gem install file_utils-1.0.1.gem
-call gem install networking-1.0.1.gem
-call gem install params-1.0.1.gem
-call gem install process_monitoring-1.0.1.gem
-call gem install run_in_background-1.0.1.gem
-call gem install validations-1.0.1.gem
-call gem install content_server-1.0.1.gem
+gem install log-1.1.0.gem
+gem install content_data-1.1.0.gem
+gem install email-1.1.0.gem
+gem install file_copy-1.1.0.gem
+gem install file_indexing-1.1.0.gem
+gem install file_monitoring-1.1.0.gem
+gem install file_utils-1.1.0.gem
+gem install networking-1.1.0.gem
+gem install params-1.1.0.gem
+gem install process_monitoring-1.1.0.gem
+gem install run_in_background-1.1.0.gem
+gem install validations-1.1.0.gem
+gem install content_server-1.1.0.gem
--- lib/params/version.rb
+++ lib/params/version.rb
@@ -1,3 +1,3 @@
 module Params
-  VERSION = "1.0.3"
+  VERSION = "1.1.0"
 end
