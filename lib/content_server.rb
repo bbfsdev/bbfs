3dfa8d83b971296bcbacaed6f5a9430479e5e090
yarondbb <yaron.dror.bb@gmail.com>
2013-07-06 23:12:09 +0300

    issue 182

--- lib/content_server.rb
+++ lib/content_server.rb
@@ -1,17 +1,2 @@
-require 'content_server/content_server'
-require 'content_server/backup_server'
-
 module ContentServer
-  # Flags combined for content and backup server.
-  Params.path('local_content_data_path', '', 'ContentData file path.')
-
-  # Monitoring
-  Params.boolean('enable_monitoring', false, 'Whether to enable process monitoring or not.')
-  Params.integer('process_vars_delay', 3, 'pulling time of variables')
-
-  # Handling thread exceptions.
-  Params.boolean('abort_on_exception', true, 'Any exception in any thread will abort the run.')
-
-  Params.integer('data_flush_delay', 300, 'Number of seconds to delay content data file flush to disk.')
-
 end # module ContentServer
