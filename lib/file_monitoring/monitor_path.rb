45eb44e3aa1341ff3ed1a1b7d6948cf4c7051a8d
yarondbb <yaron.dror.bb@gmail.com>
2013-08-31 02:15:00 +0300

    ref

--- lib/file_monitoring/monitor_path.rb
+++ lib/file_monitoring/monitor_path.rb
@@ -75,7 +75,7 @@ module FileMonitoring
           #  i=i+1
           #}
           $local_content_data_lock.synchronize{
-            $local_content_data.add_instance(@@digest.hexdigest.downcase, @size, Params['local_server_name'],
+            $local_content_data.add_instance(digest.hexdigest.downcase, @size, Params['local_server_name'],
                                              @path, @modification_time)
             #$local_content_data.add_instance(str, @size, Params['local_server_name'],
             #                                 @path, @modification_time)
