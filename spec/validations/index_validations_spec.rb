6183b306d8b77155b1833dc8d2575b4c6fd8e2e1
Unknown <genadyp@lnx-genadyp.ibm.com>
2013-09-01 01:25:29 +0300

    index_validations_spec fix

--- spec/validations/index_validations_spec.rb
+++ spec/validations/index_validations_spec.rb
@@ -67,8 +67,8 @@ module Validations
           failed.content_exists(@checksum2).should be_true
           failed.checksum_instances_size(absent_checksum).should eq(1)
           failed.checksum_instances_size(@checksum2).should eq(1)
-          failed.instance_exists(absent_path, @server, absent_checksum).should be_true
-          failed.instance_exists(@path4, @server, @checksum2).should be_true
+          failed.instance_exists(absent_path, @server).should be_true
+          failed.instance_exists(@path4, @server).should be_true
         end
       end
     end
