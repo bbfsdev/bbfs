fbc9746f13186a523c1fb16647747d6bde47b5cd
yarondbb <yaron.dror.bb@gmail.com>
2013-09-06 23:17:50 +0300

    ref

--- lib/content_server/backup_server.rb
+++ lib/content_server/backup_server.rb
@@ -102,14 +102,14 @@ module ContentServer
         written_to_file = false
         $local_content_data_lock.synchronize{
           local_content_data_unique_id = $local_content_data.unique_id
-          if (local_content_data_unique_id != last_content_data_id)
+          #if (local_content_data_unique_id != last_content_data_id)
             last_content_data_id = local_content_data_unique_id
             $local_content_data.to_file($tmp_content_data_file)
             written_to_file = true
             Log.info('End flush local content data to file.')
-          else
-            Log.info('no need to flush. content data has not changed')
-          end
+          #else
+            #Log.info('no need to flush. content data has not changed')
+          #end
         }
         File.rename($tmp_content_data_file, Params['local_content_data_path']) if written_to_file
         $testing_memory_log.info("End flush content data to file") if $testing_memory_active
