cc22b2a50a668a2a69f8e8f1ded1ff27e348a911
yarondbb <yaron.dror.bb@gmail.com>
2013-12-06 13:14:27 +0200

    fixed init of params

--- test/run_in_background/run_in_background_test.rb
+++ test/run_in_background/run_in_background_test.rb
@@ -4,7 +4,7 @@ require 'run_in_background'
 # TODO break to number of small tests according to functionality
 # TODO rewrite with Shoulda/RSpec
 class TestRunInBackground < Test::Unit::TestCase
-  #include RunInBackground
+  Param.init([])
 
   if RUBY_PLATFORM =~ /linux/ or RUBY_PLATFORM =~ /darwin/
     OS = :LINUX
