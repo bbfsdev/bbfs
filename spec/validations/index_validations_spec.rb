292b43edd98988654febab61eef95603ef935e6c
genadyp <nukegluk@gmail.com>
2014-01-21 17:09:46 +0000

    1.code coverage support 2.move all minitest tests to RSpec format 3.new_spec rake task, that creates new template spec

--- spec/validations/index_validations_spec.rb
+++ spec/validations/index_validations_spec.rb
@@ -1,3 +1,8 @@
+# NOTE Code Coverage block must be issued before any of your application code is required
+if ENV['BBFS_COVERAGE']
+  require_relative '../spec_helper.rb'
+  SimpleCov.command_name 'validations'
+end
 require 'rspec'
 require_relative '../../lib/validations.rb'
 
