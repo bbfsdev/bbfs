069abdc24c5009f9de582c17f00de2e3d504c4f2
Unknown <genadyp@lnx-genadyp.ibm.com>
2014-03-24 17:56:12 +0200

    ContentData performance spec initial

---
+++ spec/content_data/content_data_performance_spec.rb
@@ -0,0 +1,150 @@
+# NOTE Code Coverage block must be issued before any of your application code is required
+if ENV['BBFS_COVERAGE']
+  require_relative '../spec_helper.rb'
+  SimpleCov.command_name 'content_data'
+end
+
+require 'rspec'
+require 'tempfile'
+#require 'random'
+require_relative '../../lib/content_data/content_data.rb'
+
+describe 'Content Data Performance Test' do
+  before :all do
+    Params.init Array.new
+    # must preced Log.init, otherwise log containing default values will be created
+    Params['log_write_to_file'] = false
+    Log.init
+
+    NUMBER_INSTANCES = 350_000
+    MAX_CHECKSUM = NUMBER_INSTANCES  #1000
+    INSTANCE_SIZE = 1000
+    SERVER = "server"
+    PATH = "file_"
+    MTIME = 1000
+  end
+
+  context 'IO operations' do
+    it 'should create ContentData object in acceptable amount of time' do
+      LIMIT_TIME = 10*60;  # 10 minutes
+
+      build_thread = Thread.new do
+        @cd1 = ContentData::ContentData.new
+        NUMBER_INSTANCES.times do |i|
+          @cd1.add_instance(i.to_s, INSTANCE_SIZE, SERVER, PATH + i.to_s, MTIME)
+        end
+      end
+
+      timer = 0
+      timer_thread = Thread.new do
+        while (timer < LIMIT_TIME && build_thread.alive?)
+          timer += 1
+          sleep 1
+        end
+        if (build_thread.alive?)
+          Thread.kill(build_thread)
+        end
+      end
+      [build_thread, timer_thread].each { |th| th.join }
+
+      is_succeeded = timer < LIMIT_TIME
+      msg = "ContentData build for #{NUMBER_INSTANCES} " +
+        (is_succeeded ? "" : "do not ") + "finished in #{timer} seconds"
+
+      # main check
+      #timer.should be < LIMIT_TIME, msg
+      is_succeeded.should be_true
+      puts msg if is_succeeded
+
+      # checks that test was correct
+      if is_succeeded
+        @cd1.should be
+      end
+
+    end
+  end
+
+  context 'Set operations' do
+    before :all do
+      @cd1 = ContentData::ContentData.new
+      NUMBER_INSTANCES.times do |i|
+        @cd1.add_instance(i.to_s, INSTANCE_SIZE, SERVER, PATH + i.to_s, MTIME)
+      end
+      @cd2 = ContentData::ContentData.new(@cd1)
+      @cd2.add_instance((MAX_CHECKSUM+1).to_s, INSTANCE_SIZE, SERVER, PATH + "new", MTIME)
+    end
+
+
+    pending 'should consume acceptable amount of memory' do
+      puts "Memory usage test"
+      LIMIT_MEMORY = 100*1024
+
+      minus_thread = Thread.new { res_cd = ContentData.remove(@cd1, @cd2) }
+
+      memory_of_process = 0
+      memory_usage_thread = Thread.new do
+        while (memory_of_process < LIMIT_MEMORY && minus_thread.alive?)
+          memory_of_process = if Gem::win_platform?
+                                `tasklist /FI \"PID eq #{Process.pid}\" /NH /FO \"CSV\"`.split(',')[4]
+                              else
+                                `ps -o rss= -p #{Process.pid}`.to_i / 1000
+                              end
+          puts Process.pid + " => " + memory_of_process
+          sleep 1
+        end
+        if (minus_thread.alive?)
+          Thread.kill(minus_thread)
+        end
+      end
+
+      is_succeeded = memory_of_process
+      msg = "ContentData minus for #{NUMBER_INSTANCES} " +
+        (is_succeeded ? "" : "do not ") + "consume #{memory_of_process} bytes"
+
+      # main check
+      #timer.should be < LIMIT_TIME, msg
+      is_succeeded.should be_true
+      puts msg if is_succeeded
+
+      # checks that test was correct
+      if is_succeeded
+        res_cd.should be
+        res_cd.instances_size.should == 1
+      end
+    end
+
+    it 'should finish in acceptable time' do
+      LIMIT_TIME = 10*60;  # 10 minutes
+
+      res_cd = nil
+      minus_thread = Thread.new { res_cd = ContentData.remove(@cd1, @cd2) }
+
+      timer = 0
+      timer_thread = Thread.new do
+        while (timer < LIMIT_TIME && minus_thread.alive?)
+          timer += 1
+          sleep 1
+        end
+        if (minus_thread.alive?)
+          Thread.kill(minus_thread)
+        end
+      end
+      [minus_thread, timer_thread].each { |th| th.join }
+
+      is_succeeded = timer < LIMIT_TIME
+      msg = "ContentData minus for #{NUMBER_INSTANCES} " +
+        (is_succeeded ? "" : "do not ") + "finished in #{timer} seconds"
+
+      # main check
+      #timer.should be < LIMIT_TIME, msg
+      is_succeeded.should be_true
+      puts msg if is_succeeded
+
+      # checks that test was correct
+      if is_succeeded
+        res_cd.should be
+        res_cd.instances_size.should == 1
+      end
+    end
+  end
+end
