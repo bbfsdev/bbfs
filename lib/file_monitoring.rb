1b3ce9d2b0a6976598e0853355242eda7340ff38
yarondbb <yaron.dror.bb@gmail.com>
2014-08-08 13:46:47 +0300

    removing redundent log

--- lib/file_monitoring.rb
+++ lib/file_monitoring.rb
@@ -18,7 +18,6 @@ module FileMonitoring
   Params.path('default_monitoring_log_path', '~/.bbfs/log/file_monitoring.log',
               'Default path for file monitoring log file. ' \
               'This log containd track of changes found during monitoring')
-  puts "Yaron monitoring_paths 1"
   Params.complex('monitoring_paths', [], 'Array of Hashes with 3 fields: ' \
                  'path, scan_period and stable_state.')
   Params.boolean('manual_file_changes', false, 'true, indicates to application that a set of files were ' \
